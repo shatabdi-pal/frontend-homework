2bcb8e961b881b340686ee691cfdd1cd8266031f
shatabdi-pal <shatabdi@pdx.edu>
2023-04-07 15:15:07 -0700

    updated palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,34 @@
 const elem = document.querySelector('input');
 
 elem.addEventListener('input', handleInput);
+
+const output = document.getElementById("result");
+
+function handleInput(e) {
+    var number = e.target.value
+    var temp = number
+    var reverse = 0
+
+    if (number > 0)
+    {
+        while (number > 0){
+            var digit = number % 10;
+            reverse = reverse * 10 + digit
+            number = Math.floor(number/10)
+            if (temp == reverse){
+                output.textContent = "Yes. This is a palindrome!"; 
+                output.style.color ="green";
+            }
+            else{
+                output.textContent = "No. Try again"; 
+                output.style.color ="red"; 
+            }
+        }
+
+    }
+    else {
+        output.textContent = "Please enter valid positive number. input cannot be text or negative number";
+        output.style.color ="red";
+    }
+    
+}
