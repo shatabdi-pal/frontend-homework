3df77ea2aa98fba9acf0f4bf2a160fb45d80943d
shatabdi-pal <shatabdi@pdx.edu>
2023-04-13 12:45:46 -0700

    var changed with let

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -5,14 +5,14 @@ elem.addEventListener('input', handleInput);
 const output = document.getElementById("result");
 
 function handleInput(e) {
-    var number = e.target.value
-    var temp = number
-    var reverse = 0
+    let number = e.target.value
+    let temp = number
+    let reverse = 0
 
     if (number > 0)
     {
         while (number > 0){
-            var digit = number % 10;
+            let digit = number % 10;
             reverse = reverse * 10 + digit
             number = Math.floor(number/10)
             if (temp == reverse){
@@ -22,9 +22,8 @@ function handleInput(e) {
             else{
                 output.textContent = "No. Try again"; 
                 output.style.color ="red"; 
-            }
+             }
         }
-
     }
     else {
         output.textContent = "Please enter valid positive number. input cannot be text or negative number";
