a5c92bf60cbb5f511a90b7e62bb1771c50b9e8c5
shatabdi-pal <shatabdi@pdx.edu>
2023-04-22 17:08:58 -0700

    highlited word updated

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,18 @@
-// Add your code here
+// Add you code here
+//source of help:  https://tomekdev.com/posts/highlight-text-in-javascript
+
+let input = document.querySelector('#userinput');
 
 input.addEventListener('keydown', handleKeyDown);
+
+
+let paragraph = document.querySelector('.form-text');
+
+function handleKeyDown(e){
+    const word = e.target.value;
+    const regex = new RegExp(word, 'gi');
+    let text = paragraph.innerHTML;
+    text = text.replace(/(<mark class="highlight">|<\/mark>)/gim, '');
+    const newText = text.replace(regex, '<mark class="highlight">$&</mark>');
+    paragraph.innerHTML = newText;
+}
